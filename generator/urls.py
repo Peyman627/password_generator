8e513811ced5d64d8605054d25f536dc855283a6
Peyman Hassani <hassani.peyman627@gmail.com>
2020-11-28 17:43:28 +0330

    about page finished

--- generator/urls.py
+++ generator/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='index'),
     path('password/', views.password, name='password'),
+    path('about/', views.about, name='about'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,32 @@
+import random
+
 from django.shortcuts import render
 
 
 def home(request):
-    context = {'password': 'hsfsdg243*ds'}
-    return render(request, 'generator/home.html', context)
+    return render(request, 'generator/home.html')
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+    characters = list('abcdefghijklmnopqrstuvwxys')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+    password = ''
+
+    for _ in range(length):
+        password += random.choice(characters)
+
+    context = {'password': password}
+    return render(request, 'generator/password.html', context)
+
+
+def about(request):
+    return render(request, 'generator/about.html')
