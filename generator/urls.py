caae04027e35f01cf99106e64f2eab5ce49212be
Peyman Hassani <hassani.peyman627@gmail.com>
2020-11-28 17:01:19 +0330

    basic page for password created

--- generator/urls.py
+++ generator/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('password/', views.password, name='password'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def home(request):
     context = {'password': 'hsfsdg243*ds'}
     return render(request, 'generator/home.html', context)
+
+
+def password(request):
+    return render(request, 'generator/password.html')
